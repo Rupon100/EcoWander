7c8836184142267fdab0726075ec3a6234607cc6
Rupon <fardiidlamrupon@gmail.com>
2024-11-20 10:25:12 +0600

    design profile

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -9,13 +9,19 @@ const Profile = () => {
     return (
         <div className="p-8 md:p-24 flex flex-col gap-8 justify-center items-center">
             <h2 className="text-center text-2xl md:text-4xl font-semibold animate__animated animate__lightSpeedInRight">Welcome, {user.displayName}! <br /> Your EcoWander profile is ready</h2>
-            <div className="p-12 flex gap-4 flex-col justify-center items-center rounded shadow-md">
-
-                <img className="w-16 h-16 border rounded-full object-cover" src={user.photoURL} alt="user profile" />
-
-                <h2 className="text-2xl md:text-3xl font-semibold">{user.displayName}</h2>
-                <p className="font-medium text-lg">{user.email}</p>
-                <Link to='/update-profile' className="btn border-none bg-sky-600 hover:bg-sky-600 text-white">Update Information</Link>
+            <div className="relative h-72 flex flex-col justify-center items-center rounded shadow-md">
+                <div className="h-2/5 rounded-t-md w-full round  bg-gradient-to-br from-sky-600 to-sky-300">
+                </div>
+                <div className="absolute top-10">
+                   <div className="ring-primary ring-offset-base-100 w-24 rounded-full ring ring-offset-2">
+                       <img className="w-24 h-24 border rounded-full object-cover" src={user.photoURL} alt="user profile" />
+                   </div>
+                </div>
+                <div className="h-3/5 p-6 pt- text-center space-y-2">
+                   <h2 className="text-lg md:text-xl font-semibold">{user.displayName}</h2>
+                   <p className="font-medium text-base md:text-lg">{user.email}</p>
+                   <Link to='/update-profile' className="btn border-none bg-sky-600 hover:bg-sky-600 text-white">Update Information</Link>
+                </div>
             </div>
         </div>
     );
